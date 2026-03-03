fad683270d447252c4a6e5284ac0f73af957783c
vesapaci1 <vesa.paci@lowerthecurve.com>
2026-03-03 10:45:00 +0100

    Props, Parent&Child Components Practice

---
+++ Child.js
@@ -0,0 +1,5 @@
+function Child(props) {
+  return <p>Hello, {props.name}! You clicked {props.count} times.</p>;
+}
+
+export default Child;
---
+++ app.js
@@ -0,0 +1,45 @@
+// Basic Components
+// function Greeting() {
+//   return <h1>Hello, React!</h1>;
+// }
+// export default Greeting;
+
+// function Welcome() {
+//   return <h1>Hello, Vesa!</h1>;
+// }
+// export default Welcome;
+
+// Using props
+// import Welcome from './Welcome';
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Vesa" />
+//       <Welcome name="Tina" />
+//       <Welcome name="Alex" />
+//     </div>
+//   );
+// }
+// export default App;
+
+
+//Using Parent & Child Components
+import { useState } from "react";
+import Child from './Child';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>I am the parent component</h1>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+
+      <Child name="Vesa" count={count} />
+      <Child name="Tina" count={count} />
+    </div>
+  );
+}
+
+export default App;
